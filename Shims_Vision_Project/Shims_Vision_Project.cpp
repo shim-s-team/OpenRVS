b7a49e07f8091339042834f1de0fae655f044d5d
DjwChoi <jwchoi@han-mech.co.kr>
2019-06-01 11:24:09 +0900

    cmake 개발 환경 구축

---
+++ Shims_Vision_Project/Shims_Vision_Project.cpp
@@ -0,0 +1,12 @@
+﻿// Shims_Vision_Project.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "Shims_Vision_Project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
