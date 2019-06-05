fe536ed0b6394dc596c0c8319182b254c68efbfb
DjwChoi <jwchoi@han-mech.co.kr>
2019-06-05 17:52:16 +0900

    Build a development environment tailored to various os environments

--- Shims_Vision_Project/test_apple/test_apple.cpp
+++ Shims_Vision_Project/test_apple/test_apple.cpp
@@ -1,13 +1,13 @@
 ﻿// Shims_Vision_Project.cpp : 애플리케이션의 진입점을 정의합니다.
 //
 
-#include "Shims_Vision_Project.h"
+#include "../include/test.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello Develop." << endl;
-	cout << "New Data +1" << endl;
+	cout << "Shim\'s Vision Project Test" << endl;
 	return 0;
 }
+
